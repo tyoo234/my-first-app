fc23d69364ab2930f8fe2feed6d166cffd49f8e9
tyoo234 <prasetyoo234@gmail.com>
2024-09-25 15:20:23 +0700

    master

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [ "./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        text: "#9b9b9b",
+        orange: "#FB644B",
+      },
+    },
+  },
+  plugins: [],
+}
+
