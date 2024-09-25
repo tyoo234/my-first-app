0a612e998cfc36c06279fa44af0a9a2b15227141
tyoo234 <prasetyoo234@gmail.com>
2024-09-25 18:42:54 +0700

    master

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,32 @@
 import React from "react";
 import { navLink } from "../data/dummy";
 import logo from "../asset/logo.svg";
+import { Link } from "react-scroll";
 
 const Navbar = () => {
-      return <div>
-                        <div>
-                              <img src={logo} alt="" />
-                        </div>
-                        <ul>
-                              <li></li>
-                        </ul>
-                  </div>;
+      return (
+      <div className="flex" justify-between items-center w-full fixed top-0 px-72px h-80px>
+            <div>
+                  <img src={logo} alt="" />
+            </div>
+            <ul>
+                  <li>
+                        {navLink.map((item) => {
+                              return <Link to={item.id} key={item.id}>
+                                    {item.name}
+                              </Link>;
+                        })
+                        }
+                  </li>
+            </ul>
+            <div>
+                  <button className="px-6 lg:px-8 py-2 bg-white outline-none 
+                                                      hover:bg-orange hover:text-white rounded-full">
+                        Sign In
+                  </button>
+            </div>
+      </div>
+      );
 };
 
 export default Navbar;
