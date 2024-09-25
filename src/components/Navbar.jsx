cd30e807d13423d8a0293d894a75dcb4e2a977fc
tyoo234 <prasetyoo234@gmail.com>
2024-09-25 17:37:00 +0700

    master

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,18 @@
 import React from "react";
+import { navLink } from "../data/dummy";
+import logo from "../asset/logo.svg";
 
 const Navbar = () => {
-      return <div>Navbar</div>;
+      return <div>
+                        <div>
+                              <img src={logo} alt="" />
+                        </div>
+                        <ul>
+                              <li>
+                                    <link></link>
+                              </li>
+                        </ul>
+                  </div>;
 };
 
 export default Navbar;
