8502be7625bd7cd7480376cbc4d5009f019e3ced
tyoo234 <prasetyoo234@gmail.com>
2024-09-25 21:25:34 +0700

    master

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,27 +2,33 @@ import React from "react";
 import { navLink } from "../data/dummy";
 import logo from "../asset/logo.svg";
 import { Link } from "react-scroll";
+import { IoIosMenu } from "react-icons/io";
 
 const Navbar = () => {
       return (
-            <div className="flex px-[72px] h-[80px] justify-between items-center w-full fixed top-0">
-                  <div>
-                        <img src={logo} alt="" />
+            <div className="flex px-5 lg:px-[72px] h-[80px] justify-between items-center w-full fixed top-0">
+                  <div className="w-[103px] h-[25px]">
+                        <img src={logo} alt="" className="w-full h-full object-contain"/>
                   </div>
-                  <ul>
-                        <li>
+                  <ul className="hidden md:flex">
+                        <li className="flex gap-x-4">
                               {navLink.map((item) => {
-                                    return <Link to={item.id} key={item.id}>
-                                          {item.name}
-                                    </Link>;
+                                    return (
+                                    <Link to={item.id} 
+                                                key={item.id} 
+                                                className="hover:text-orange text-sm md:text-md">
+                                                {item.name}
+                                    </Link>
+                              );
                               })
                               }
                         </li>
                   </ul>
-                  <div>
+                  <div className="flex items-center">
                         <button className="px-6 lg:px-8 py-2 bg-white outline-none hover:bg-orange hover:text-white rounded-full">
                               Sign In
                         </button>
+                              <IoIosMenu className="text-2xl blok md:hidden"/>
                   </div>
             </div>
       );
