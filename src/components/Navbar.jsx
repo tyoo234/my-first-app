ab9d4c472478dd25d64e53c49bc473a1eecd0f2a
tyoo234 <prasetyoo234@gmail.com>
2024-09-25 20:41:15 +0700

    master

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,27 +5,26 @@ import { Link } from "react-scroll";
 
 const Navbar = () => {
       return (
-      <div className="flex" justify-between items-center w-full fixed top-0>
-            <div>
-                  <img src={logo} alt="" />
+            <div className="flex px-[72px] h-[80px] justify-between items-center w-full fixed top-0">
+                  <div>
+                        <img src={logo} alt="" />
+                  </div>
+                  <ul>
+                        <li>
+                              {navLink.map((item) => {
+                                    return <Link to={item.id} key={item.id}>
+                                          {item.name}
+                                    </Link>;
+                              })
+                              }
+                        </li>
+                  </ul>
+                  <div>
+                        <button className="px-6 lg:px-8 py-2 bg-white outline-none hover:bg-orange hover:text-white rounded-full">
+                              Sign In
+                        </button>
+                  </div>
             </div>
-            <ul>
-                  <li>
-                        {navLink.map((item) => {
-                              return <Link to={item.id} key={item.id}>
-                                    {item.name}
-                              </Link>;
-                        })
-                        }
-                  </li>
-            </ul>
-            <div>
-                  <button className="px-6 lg:px-8 py-2 bg-white outline-none 
-                                                      hover:bg-orange hover:text-white rounded-full">
-                        Sign In
-                  </button>
-            </div>
-      </div>
       );
 };
 
