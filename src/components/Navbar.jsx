bbe1ec7ccf58912c74651fbe47c8464627e3856f
tyoo234 <prasetyoo234@gmail.com>
2024-09-25 22:58:51 +0700

    master

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,7 @@ import { IoIosMenu, IoMdClose } from "react-icons/io";
 
 const Navbar = () => {
       return (
-            <div className="flex px-5 lg:px-[72px] h-[80px] justify-between items-center w-full fixed top-0">
+            <div className="flex justify-between items-center px-5 lg:px-[72px] h-[80px] w-full fixed top-0">
                   <div className="w-[103px] h-[25px]">
                         <img src={logo} alt="" className="w-full h-full object-contain"/>
                   </div>
@@ -16,7 +16,7 @@ const Navbar = () => {
                                     return (
                                     <Link to={item.id} 
                                                 key={item.id} 
-                                                className="hover:text-orange text-sm md:text-md">
+                                                className="hover:text-orange text-sm md:text-md cursor-pointer">
                                                 {item.name}
                                     </Link>
                               );
@@ -28,24 +28,24 @@ const Navbar = () => {
                         <button className="px-6 lg:px-8 py-2 bg-white outline-none hover:bg-orange hover:text-white rounded-full">
                               Sign In
                         </button>
-                              <IoIosMenu className="text-2xl blok md:hidden"/>
+                              <IoIosMenu className="text-2xl block md:hidden"/>
                   </div>
-                  <div className="flex flex-col gap-4 pt-5 px-5 md:hidden absolute top-0 right-sm">
-                  <IoMdClose />
-                  <ul className="">
-                        <li className="flex gap-y-4 flex-col">
-                              {navLink.map((item) => {
-                                    return (
-                                    <Link to={item.id} 
+                  <div className="flex flex-col gap-4 pt-5 px-5 md:hidden absolute top-0 right-sm w-full h-screen bg-white">
+                        <IoMdClose className="cursor-pointer text-2xl"/>
+                        <ul className= "">
+                              <li className="flex gap-y-4 flex-col">
+                                    {navLink.map((item) => {
+                                          return (
+                                          <Link to={item.id} 
                                                 key={item.id} 
-                                                className="hover:text-orange text-sm md:text-md border-b">
+                                                className="hover:text-orange text-sm md:text-md border-b cursor-pointer">
                                                 {item.name}
-                                    </Link>
-                              );
-                              })
-                              }
-                        </li>
-                  </ul>
+                                          </Link>
+                                                 );
+                                          })
+                                    }
+                              </li>
+                        </ul>
                   </div>
             </div>
       );
