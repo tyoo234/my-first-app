d6db847a97476910b183922f68310a16a954519b
tyoo234 <prasetyoo234@gmail.com>
2024-09-26 15:02:31 +0700

    master

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import { navLink } from "../data/dummy";
 import logo from "../asset/logo.svg";
 import { Link } from "react-scroll";
 import { IoIosMenu, IoMdClose } from "react-icons/io";
 
 const Navbar = () => {
+      const [nav, setNav] = useState(false);
       return (
             <div className="flex justify-between items-center px-5 lg:px-[72px] h-[80px] w-full fixed top-0">
                   <div className="w-[103px] h-[25px]">
@@ -28,10 +29,11 @@ const Navbar = () => {
                         <button className="px-6 lg:px-8 py-2 bg-white outline-none hover:bg-orange hover:text-white rounded-full">
                               Sign In
                         </button>
-                              <IoIosMenu className="text-2xl block md:hidden"/>
+                              <IoIosMenu className="text-2xl block md:hidden" 
+                              onClick={() => setNav(prev=>!prev)}/>
                   </div>
-                  <div className="flex flex-col gap-4 pt-5 px-5 md:hidden absolute top-0 right-sm w-full h-screen bg-white">
-                        <IoMdClose className="cursor-pointer text-2xl"/>
+                  <div className={'${ nav ? "right-0":"-right-[100%]"} flex flex-col gap-4 pt-5 px-5 md:hidden absolute top-0 w-full h-screen bg-white'}>
+                        <IoMdClose className="cursor-pointer text-2xl" onClick={() => setNav(prev=>!prev)}/>
                         <ul className= "">
                               <li className="flex gap-y-4 flex-col">
                                     {navLink.map((item) => {
