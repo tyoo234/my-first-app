aea405ccd8499afc726fe1c17f445a44385bfc8b
tyoo234 <prasetyoo234@gmail.com>
2024-09-26 16:05:01 +0700

    master

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,18 +9,18 @@ const Navbar = () => {
       return (
             <div className="flex justify-between items-center px-5 lg:px-[72px] h-[80px] w-full fixed top-0">
                   <div className="w-[103px] h-[25px]">
-                        <img src={logo} alt="" className="w-full h-full object-contain"/>
+                        <img src={logo} alt="" className="w-full h-full object-contain" />
                   </div>
                   <ul className="hidden md:flex">
                         <li className="flex gap-x-4">
                               {navLink.map((item) => {
                                     return (
-                                    <Link to={item.id} 
-                                                key={item.id} 
+                                          <Link to={item.id}
+                                                key={item.id}
                                                 className="hover:text-orange text-sm md:text-md cursor-pointer">
                                                 {item.name}
-                                    </Link>
-                              );
+                                          </Link>
+                                    );
                               })
                               }
                         </li>
@@ -29,22 +29,24 @@ const Navbar = () => {
                         <button className="px-6 lg:px-8 py-2 bg-white outline-none hover:bg-orange hover:text-white rounded-full">
                               Sign In
                         </button>
-                              <IoIosMenu className="text-2xl block md:hidden" 
-                              onClick={() => setNav(prev=>!prev)}/>
+                        <IoIosMenu
+                              className="text-2xl block md:hidden"
+                              onClick={() => setNav((prev) => !prev)} />
                   </div>
-                  <div className={'${ nav ? "right-0":"-right-[100%]"} flex flex-col gap-4 pt-5 px-5 md:hidden absolute top-0 w-full h-screen bg-white'}>
-                        <IoMdClose className="cursor-pointer text-2xl" onClick={() => setNav(prev=>!prev)}/>
-                        <ul className= "">
+                  <div
+                        className={'${ nav ? "right-0" : "-right-[100%]"} flex flex-col gap-4 pt-5 px-5 md:hidden absolute top-0 w-full h-screen bg-white'}>
+                        <IoMdClose className="cursor-pointer text-2xl" onClick={() => setNav((prev) => !prev)} />
+                        <ul className="">
                               <li className="flex gap-y-4 flex-col">
                                     {navLink.map((item) => {
                                           return (
-                                          <Link to={item.id} 
-                                                key={item.id} 
-                                                className="hover:text-orange text-sm md:text-md border-b cursor-pointer">
-                                                {item.name}
-                                          </Link>
-                                                 );
-                                          })
+                                                <Link to={item.id}
+                                                      key={item.id}
+                                                      className="hover:text-orange text-sm md:text-md border-b cursor-pointer">
+                                                      {item.name}
+                                                </Link>
+                                          );
+                                    })
                                     }
                               </li>
                         </ul>
