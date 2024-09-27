b95dcf68b66a3afe2df398e3554e058285df1d90
tyoo234 <prasetyoo234@gmail.com>
2024-09-27 10:36:54 +0700

    master

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,7 +7,7 @@ const Home = () => {
                         <h1 className="font-bold text-5xl">Let's Find a Best Doctor Around The World</h1>
                         <h1 className="text-text max-w-[500px]">We develop an app to allow you to improve your healt better and get a life you want to life</h1>
                         <div>
-                              <button className="px-6 lg:px-8 py-2 bg-orange text-white outline-none hover:bg-black rounded-full">
+                              <button className="px-6 lg:px-8 py-2 bg-orange text-white outline-none hover:bg-gray-600 rounded-full">
                                     Learn More
                               </button>
                         </div>
--- src/components/Step.jsx
+++ src/components/Step.jsx
@@ -3,18 +3,22 @@ import { features } from "../data/dummy";
 
 const Step = () => {
   return (
-    <div>
-      <h1>3 STPES QUICK</h1>
-      <h1>Claim Your Better Health</h1>
-      {features.map((item, i)=>{
-            return <div>
-                  <div>
-                        <img src="" alt="" />
-                  </div>
-                  <h1>{item.title}</h1>
-                  <p>{item.desc}</p>
+    <div className="">
+      <h1 className="text-orange font-bold text-xl">3 STPES QUICK</h1>
+      <h1 className="font-bold text-3xl">Claim Your Better Health</h1>
+      <div className="flex flex-row gap-x-4 w-[40] h-[40] justify-center item-center h-screen">
+        {features.map((item, i) => {
+          return <div className="flex flex-col">
+            <div>
+              <img src={item.image} alt="" />
             </div>
-      })}
+            <div className="text-center text-2xl tracking-wide h-screen">
+              <h1 className="font-bold text-black">{item.title}</h1>
+              <p>{item.desc}</p>
+            </div>
+          </div>
+        })}
+      </div>
     </div>
   )
 }
